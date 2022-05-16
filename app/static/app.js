ae0b70dada56f001ab5d0aa2c1d75c21fc26b0c7
Valerie Liao <valerieliao@Valeries-MacBook-Air.local>
2022-05-16 21:45:37 +0800

    shuffle function changed

--- app/static/app.js
+++ app/static/app.js
@@ -133,20 +133,7 @@ function generateSudoku() {
 
 function shuffle(grid) {
 
-	var i, j, k, temp, col, col1, col2,
-	row1, row2, sub, sub1, sub2, num1, num2;
-
-	//swap the same columns of each subsquare
-	for(i = 0; i < 25; i++) {
-		col = Math.floor(Math.random()*3);
-		sub1 = Math.floor(Math.random()*3);
-		sub2 = Math.floor(Math.random()*3);
-		for(j = 0; j < grid.length; j++) {
-			temp = grid[j][col + sub1*3];
-			grid[j][col + sub1*3] = grid[j][col + sub2*3];
-			grid[j][col + sub2*3] = temp;
-		}
-	}
+	var i, j, temp,  col1, col2, row1, row2, sub;
 
 	//swap all columns within each subsquare
 	for(i = 0; i < 25; i++) {
@@ -159,7 +146,8 @@ function shuffle(grid) {
 			grid[j][sub*3 + col1] = grid[j][sub*3 + col2];
 			grid[j][sub*3 + col2] = temp;
 		}
-	}
+	}  
+	
 
 	//swap all rows within each subsquare
 	for(i = 0; i < 25; i++) {
@@ -172,22 +160,8 @@ function shuffle(grid) {
 			grid[sub*3 + row1][j] = grid[sub*3 + row2][j];
 			grid[sub*3 + row2][j] = temp;
 		}
-	}
+	}  
 
-	//swap one number with another
-	for(i = 0; i < 25; i++) {
-		num1 = Math.floor(Math.random()*9 + 1);
-		num2 = Math.floor(Math.random()*9 + 1);
-		while(num1 == num2) num2 = Math.floor(Math.random()*9 + 1);
-		for(j = 0; j < grid.length; j++) {
-			for(k = 0; k < grid[j].length; k++) {
-				if(grid[j][k] == num1)
-					grid[j][k] = num2;
-				else if(grid[j][k] == num2)
-					grid[j][k] = num1;
-			}
-		}
-	}
 }
 
 function hideTiles(aGrid, hiddenGrid) {
@@ -195,9 +169,9 @@ function hideTiles(aGrid, hiddenGrid) {
 	// Randomly hide tiles, no guarantee for a unique solution
 	var numTiles, k;
 
-	for(var c = 0; c < 9; c++) {
-		for(var d = 0; d < 9; d++) {
-			hiddenGrid[c][d] = aGrid[c][d];
+	for(var i = 0; i < 9; i++) {
+		for(var j = 0; j < 9; j++) {
+			hiddenGrid[i][j] = aGrid[i][j];
 		}
 	}
 
@@ -279,7 +253,7 @@ function solve() {
 	for(var i = 0; i < 9; i++) {
 		for(var j = 0; j < 9; j++) {
 			var tile = document.getElementById("t" + i + "x" + j);
-			tile.innerHTML = puzzle.getSolution(i, j);
+			tile.innerHTML = puzzle.getSolution(i, j);	
 		}
 	}
 } 
