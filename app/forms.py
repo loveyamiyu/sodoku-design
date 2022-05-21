d4b819bb17370a7875e0901ecb4f03cdc8755035
Valerie Liao <valerieliao@Valeries-MacBook-Air.local>
2022-05-21 17:52:47 +0800

    added logout for authenticated user

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm
 from sqlalchemy import false
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -23,19 +23,7 @@ class LoginForm(FlaskForm):
             self.password.errors.append('Invalid password')
             return False
         return True
-    """
-    def validate(self):
-        initial_validation = super(LoginForm, self).validate()
-        if not initial_validation:
-            return False
-        user = User.query.filter_by(username=self.username.data).first()
-        if not user:
-            raise ValidationError('Invalid username')
-        if not user.check_password(self.password.data):
-            raise ValidationError('Invalid password')
-        return True
 
-"""
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -43,24 +31,7 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
-    """
-    def validate_username(self, username):
 
-        user = User.query.filter_by(username = username.data).first() 
-
-        if user is not None:
-
-            raise ValidationError("Username already registered, please pick a different one")
-
-    def validate_email(self, email):
-
-        user = User.query.filter_by(email = email.data).first() 
-
-        if user is not None:
-
-            raise ValidationError("Email already in use, please try a different one or reset password")
-
-    """
     def validate(self):
         initial_validation = super(RegistrationForm, self).validate()
         if not initial_validation:
